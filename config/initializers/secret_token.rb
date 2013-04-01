8dd68effc2348ae8e4f1775dba63751fa1b03330
Ubuntu <ubuntu@node1.(none)>
2013-04-01 08:20:37 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobless::Application.config.secret_token = '2b14afe28bc1567ac8833e2f9b717ff86bb4f6f090285253bf113b447cfa183f3e82906ac52ee27c3b37a2e6ab1e9ade94d6b9584580df191d10d72e73d8ff7d'
