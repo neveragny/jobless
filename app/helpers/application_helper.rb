aa13e0200864cd455543b4bfc85bcec55e04374e
Nikita Demeshchenko <neveragny@gmail.com>
2013-04-12 18:15:27 +0300

    alerts, translations, etc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,4 +60,19 @@ module ApplicationHelper
     end
   end
 
+  def twitterized_type(type)
+    case type
+      when :alert
+        "alert-block"
+      when :error
+        "alert-error"
+      when :notice
+        "alert-info"
+      when :success
+        "alert-success"
+      else
+        type.to_s
+    end
+  end
+
 end
